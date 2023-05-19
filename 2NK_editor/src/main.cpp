4f26135495d8baaefc2ad22a14d7860ff71b0654
Flipfil <hronfili@gmail.com>
2023-05-19 20:25:44 +0200

    include paths, engine-editor split
    Made include paths in projects in premake.lua. Split engine and editor as different projects. Added some comments to tools and made error checks within the toolchain

--- 2NK_editor/src/main.cpp
+++ 2NK_editor/src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "2NK_engine/engine.h"
 
 int main (int argc, char* argv[])
 {
     std::cout << "Hello" << std::endl;
+    int a = Add(10, 5);
+    int b = Sub(10, 5);
+    std::cout << a << ", " << b << std::endl;
+    GetInfo();
+    return 0;
 }
--- cli.py
+++ cli.py
@@ -1,26 +1,32 @@
-# cli build
-# cli run
-# cli generate
-# cli version
-# cli gen build run
-
 import os, sys
 import subprocess
 
 TOOLS_DIR = "tools"
 
+# comand runner script
+# usage: python3 ./cli.py [command1] [command2] ...
+# available commands are in ./tools directory as separate python scripts
+
+# note: dont use full script file as command use "run" not "run.py"
+
+# execute the command
 def RunCmd(cmd):
+    ret = 0
     script = "{}/{}/{}.py".format(os.getcwd(), TOOLS_DIR, cmd)
 
-    if os.path.exists(script):
+    if os.path.exists(script): # check valid command
         print("Executing: ", cmd)
-        subprocess.call(["python3", script])
-        print("Finished: ", cmd)
+        ret = subprocess.call(["python3", script])
     else:
         print("Invalid command: ", cmd)
+        ret = -1
 
+    return ret
+
+# execute commands in order
 for i in range(1,len(sys.argv)):
     cmd = sys.argv[i]
 
     print("\n--------------------")
-    RunCmd(cmd)
+    if RunCmd(cmd) != 0:
+        break
